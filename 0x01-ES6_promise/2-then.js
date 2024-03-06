d8fbc6b5ce9e4974b039ea1f4dbd242abba4f928
emmanuel tindi <emmanueltindi66@gmail.com>
2024-03-06 23:01:09 +0300

    file update

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,6 @@
 export default function handleResponseFromAPI(promise){
-    promise.then(() => {
-        console.log("Got a response from the API");
-        return ({status: 200, body: "success"});
-    }).catch(() => {
-        return Promise.reject({});
-    });
+    return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
